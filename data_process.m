9908b30d79ba9ac270b6db67a2c920f2f924c45f
lyh <2256551717@qq.com>
2018-07-11 11:24:42 +0800

    test

--- data_process.m
+++ data_process.m
@@ -3,7 +3,21 @@ test_flag = 0;
 if test_flag == false
     filename = '..\data\01_24\0101_0124.dat';
 else
-    filename = 'E:\work\锟斤拷锟斤拷wifi锟斤拷锟斤拷锟斤拷锟斤拷锟绞讹拷锟絓data\csi_mat\falling\falling_cdj_20140511_011.mat';
+    filename = 'E:\work\基于wifi的室内身份识别\data\csi_mat\falling\falling_cdj_20140511_011.mat';
+end    
+
+pca_l = 3;   
+dwt_low = 7;
+dwt_up = 3;
+[r, pca, h, T] = data_exchange(filename, pca_l, dwt_up, dwt_low, true, test_flag);   %r 离散小波处理结果， T时间长度
+
+%[action_list, a_i] = waveform_slice(pca, T, true);%波形切割
+
+%将片段切分投影到离散小波变换结果
+% figure(5)
+% for i = dwt_up : dwt_low
+%     subplot(dwt_low-dwt_up+1, 1, i-dwt_up+1);
+%     s = (i-dwt_up)*T;
 %     plot(r(s+1:s+T), 'b')
 %     flag = true;
 %     for j = 1 : a_i
